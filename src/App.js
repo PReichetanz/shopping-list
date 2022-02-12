deb2b8756da2166e8729c7e30000ebddacb98eb0
PReichetanz <paul.reichetanz@gmx.de>
2022-02-12 14:32:45 +0100

    Add fetch and search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import styled from 'styled-components';
+import SearchInput from './components/Search.js';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  return <Container>hello world!</Container>;
+  const [data, setData] = useState(['hello', 'huhu']);
+  console.log(data);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <SearchInput fetchedItems={data}></SearchInput>
+    </Container>
+  );
+
+  async function fetchData() {
+    const response = await fetch(
+      'https://fetch-me.vercel.app/api/shopping/items'
+    );
+    const newData = await response.json();
+    if (response.status === 200) {
+      setData(newData.data);
+    } else {
+      console.error('Opps, something went wrong!');
+    }
+  }
 }
 
 const Container = styled.main`
-  color: blue;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  gap: 0.5rem;
 `;
