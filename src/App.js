be8d60b779d6f02e9f2235e50940d94208cea0d0
PReichetanz <paul.reichetanz@gmx.de>
2022-02-14 15:19:47 +0100

    Add useToggle hook

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 import SearchInput from './components/Search.js';
 import ShoppingList from './components/ShoppingList.js';
 import useItems from './hooks/useItems.js';
+import useToggle from './hooks/useToggle.js';
 
 export default function App() {
   const { activeItems, addToActiveItems, deleteActiveItem, data } = useItems();
+  const [isShoppingDone, setIsShoppingDone] = useToggle();
 
   return (
     <Container>
@@ -13,6 +15,10 @@ export default function App() {
         onButtonClick={addToActiveItems}
         fetchedItems={data}
       ></SearchInput>
+      <h2>Warst Du heute schon einkaufen?</h2>
+      <button onClick={setIsShoppingDone}>
+        {isShoppingDone ? 'Ja! 👍' : 'Nein... 😣'}
+      </button>
     </Container>
   );
 }
